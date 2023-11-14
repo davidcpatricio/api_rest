e08dca443ba28421ee419528a14ca6eb9aa2a0d8
David Patrício <david.c.patricio@gmail.com>
2023-11-14 18:01:12 +0000

    removed DEFAULT_AUTHENTICATION_CLASSES from settings.py

--- contratos/views.py
+++ contratos/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 from rest_framework.response import Response
-
 from contratos.serializers import (EntidadeSerializer, MoradaSerializer,
                                    ContratoSerializer,
                                    EntidadeDeContratoSerializer)
@@ -10,7 +10,7 @@ from contratos.models import Entidade, Morada, Contrato, EntidadeDeContrato
 class EntidadeViewSet(viewsets.ModelViewSet):
     queryset = Entidade.objects.all().order_by('nome')
     serializer_class = EntidadeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_paginated_response(self, data):
         return Response(data)
@@ -19,7 +19,7 @@ class EntidadeViewSet(viewsets.ModelViewSet):
 class MoradaViewSet(viewsets.ModelViewSet):
     queryset = Morada.objects.all().order_by('rua')
     serializer_class = MoradaSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_paginated_response(self, data):
         return Response(data)
@@ -28,7 +28,7 @@ class MoradaViewSet(viewsets.ModelViewSet):
 class ContratoViewSet(viewsets.ModelViewSet):
     queryset = Contrato.objects.all().order_by('data_inicio')
     serializer_class = ContratoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_paginated_response(self, data):
         return Response(data)
@@ -37,7 +37,7 @@ class ContratoViewSet(viewsets.ModelViewSet):
 class EntidadeDeContratoViewSet(viewsets.ModelViewSet):
     # queryset = EntidadeDeContrato.objects.all().order_by('entidade')
     serializer_class = EntidadeDeContratoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         contrato_id = self.kwargs.get('contrato_id', None)
